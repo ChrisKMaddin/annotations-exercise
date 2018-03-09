c414efbe77ceaa4a2f4d164984f9fa0369b75678
chriskmaddin <chriskmaddin@gmail.com>
2018-03-09 12:21:16 -0600

    updated Author

--- src/Author.java
+++ src/Author.java
@@ -29,7 +29,8 @@ public class Author extends Person {
     }
 
     @Override
-    public String sortName() {
+    public String fullName() {
+
         return String.format("%s, %s", lastName, firstName);
     }
 }
